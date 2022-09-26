59e7d01fc724aff79d79ace7ff97ba19821e5f91
Bassel Amgad <bamgad7@gmail.com>
2022-09-27 00:36:32 +0200

    Add project files.

---
+++ ServerJWT/Models/AuthenticatedResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Models
+{
+    public class AuthenticatedResponse
+    {
+        public string? Token { get; set; }
+        public string? RefreshToken { get; set; }
+        public string? UserName { get; set; }
+    }
+}
