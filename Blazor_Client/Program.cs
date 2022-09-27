954ef4b6aec43df31a11b0a78ed023cf24f1a634
Bassel Amgad <bamgad7@gmail.com>
2022-09-27 22:51:51 +0200

    Injected HttpClient

--- Blazor_Client/Program.cs
+++ Blazor_Client/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration.GetConnectionString("LocalhostServer"))});
 
 await builder.Build().RunAsync();
